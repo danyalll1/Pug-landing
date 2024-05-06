4907547ca9a31f20b7e0980c5ef4a0b02ebb6d99
danyalll <116565339+danyalll1@users.noreply.github.com>
2024-05-06 12:23:59 +0300

    Delete scripts directory

--- scripts/catalog-nested/card.js
+++
@@ -1,7 +0,0 @@
-import {cardMouseMoveEventHandler} from "@/scripts/helpers/card-image.js";
-window.addEventListener('load', () => {
-    if(document.querySelector('.catalog-nested__item')){
-        const cards = document.querySelectorAll('.catalog-nested__item-images')
-        cards.forEach(el=> el.addEventListener('mousemove', (event)=> cardMouseMoveEventHandler(event,el,'catalog-nested__item-image','data-id')))
-    }
-})
--- scripts/global/preloader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const preloader = document.querySelector('.preloader-container')
-    document.body.classList.add('fixed')
-    preloader.classList.add('active')
-    window.setTimeout(function () {
-        preloader.classList.remove('active')
-        document.body.classList.remove('fixed');
-    }, 300);
-})
